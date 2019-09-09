7336f858136ca717d395625be7f975203fb2bd7c
OlgaSavytska <o.savytska77@gmail.com>
2019-09-09 17:43:27 +0300

    Updates

---
+++ precache-manifest.fb2d7e3fbbba24cf9262cce16fd244c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb01b8523ce4c9c65c488cbd23acf15",
+    "url": "/OlgaSavytska/goit-react-hw-02-movies-new/index.html"
+  },
+  {
+    "revision": "c139995c4da0f778bdd9",
+    "url": "/OlgaSavytska/goit-react-hw-02-movies-new/static/css/main.141a765b.chunk.css"
+  },
+  {
+    "revision": "f2be40fa08f548757245",
+    "url": "/OlgaSavytska/goit-react-hw-02-movies-new/static/js/2.07ba98d0.chunk.js"
+  },
+  {
+    "revision": "c139995c4da0f778bdd9",
+    "url": "/OlgaSavytska/goit-react-hw-02-movies-new/static/js/main.cca1028f.chunk.js"
+  },
+  {
+    "revision": "3f4eece43ca3ba08ed2f",
+    "url": "/OlgaSavytska/goit-react-hw-02-movies-new/static/js/runtime~main.f24e1812.js"
+  }
+]);
